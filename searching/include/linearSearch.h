860ceb5d9a526205843743a2675eb4a545066956
Nikhil Kulkarni <nikhilkulkarni88@gmail.com>
2018-01-07 17:57:51 -0800

    Adding linear Search Code and Makefile
    Signed-off-by: Nikhil Kulkarni <nikhilkulkarni88@gmail.com>

---
+++ searching/include/linearSearch.h
@@ -0,0 +1,11 @@
+/*********************************
+
+File:   linearSearch.h
+Date:   6 Jan 2018
+Author: nikhilkulkarni88@gmail.com 
+
+**********************************/
+
+#include <stdio.h>
+#include <stdint.h>
+void linearSearch (const int array[], uint32_t max, int32_t elem);
